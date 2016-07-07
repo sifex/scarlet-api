b1189c25fc5f601c56253113aa2804a179eaa66e
Alex Sinnott <chess2ryme@gmail.com>
2016-07-07 13:43:38 +1000

    Install Post fixes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,53 +7,55 @@ use Log, DB;
 
 class UserController extends Controller
 {
-  public function add($username, $clanID) {
+    public function add($username, $clanID) {
 
-    $user = DB::table('scar_users')->where('username', $username)->get();
-    $key = md5(strtolower($username) . "E6hJ9X2AptWH6bqU32");
-    if($user != null)
-    {
-      echo "User " . $username . " already in database";
+        $user = DB::table('scar_users')->where('username', $username)->get();
+        $key = md5(strtolower($username) . "E6hJ9X2AptWH6bqU32");
+
+        if($user != null) {
+            echo "User " . $username . " already in database";
+        }
+        elseif(DB::table('scar_users')->insert(
+        [
+            'username' => $username,
+            'key' => $key,
+            'clanid' => $clanID
+        ]))
+        {
+            $content = $username . " (" . $key . ") - " . $clanID;
+            return response($content)->header('Access-Control-Allow-Origin', '*');
+
+            // Log
+            Log::info('Created User: ' . $username . " (" . $key . ") - " . $clanID);
+
+        }
     }
-    elseif(DB::table('scar_users')->insert(
-    [
-      'username' => $username,
-      'key' => $key,
-      'clanid' => $clanID
-    ]))
+
+    public function info($var, $return)
     {
-      echo $username . " (" . $key . ") - " . $clanID;
+        // Log
+        Log::info('Fetching info(' . $var . ') for: ' . $return);
 
-      // Log
-      Log::info('Created User: ' . $username . " (" . $key . ") - " . $clanID);
-
+        if(DB::table('scar_users')->where('key', $var)->first())
+        {
+            $content = DB::table('scar_users')->where('key', $var)->first()->$return;
+            return response($content)->header('Access-Control-Allow-Origin', '*');
+        }
     }
-  }
 
-  public function info($var, $return)
-  {
-    // Log
-    Log::info('Fetching info(' . $var . ') for: ' . $return);
-
-    if(DB::table('scar_users')->where('key', $var)->first())
+    public function install(Request $request, $key)
     {
-      $content = DB::table('scar_users')->where('key', $var)->first()->$return;
-      return response($content)->header('Access-Control-Allow-Origin', '*');
+        // Log
+        Log::info('Installing Directory for ' . $key . ': ' . $request->installDir);
+
+        if(DB::table('scar_users')->where('key', $key)->first())
+        {
+            DB::table('scar_users')->where('key', $key)->update(['installDir'=>$request->installDir]);
+            return response('')->header('Access-Control-Allow-Origin', '*');
+        }
     }
-  }
 
-  public function install(Request $request, $key)
-  {
-    // Log
-    Log::info('Installing Directory for ' . $key . ': ' . $request->installDir);
-
-    if(DB::table('scar_users')->where('key', $key)->first())
-    {
-      DB::table('scar_users')->where('key', $key)->update(['installDir'=>$request->installDir]);
+    public function post(Request $request) {
+        echo $request->name;
     }
-  }
-
-  public function post(Request $request) {
-    echo $request->name;
-  }
 }
