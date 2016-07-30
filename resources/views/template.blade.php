25daaddc2fc4bf2f81c51b30316d9e1473b3d75e
Alex Sinnott <chess2ryme@gmail.com>
2016-07-30 12:52:18 +1000

    Font Changes

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -9,7 +9,8 @@
     <link rel="stylesheet" href="/scarlet-global/styles/scarlet-global.css">
     <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
-
+    <link href="https://fonts.googleapis.com/css?family=Exo+2:200,300,400,500" rel="stylesheet">
+    
     @yield('styles')
 
 </head>
