52fadfb80e9811d5209cbb406323bc71c712bb3f
Alex Sinnott <chess2ryme@gmail.com>
2016-10-09 13:10:55 +1100

    Testing this for IP

--- app/Http/Middleware/CrossOriginAnywhere.php
+++ app/Http/Middleware/CrossOriginAnywhere.php
@@ -17,7 +17,9 @@ class CrossOriginAnywhere
     public function handle($request, Closure $next)
     {
         return $next($request)
-                    ->header('Access-Control-Allow-Origin', '*')
-                    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->header('Access-Control-Allow-Origin', $_SERVER['HTTP_ORIGIN'])
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Max-Age', '1000')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
     }
 }
