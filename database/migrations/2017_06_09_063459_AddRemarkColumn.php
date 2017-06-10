4476eae2d670408e2d9689953cef44b4cccac4c2
Alex <chess2ryme@gmail.com>
2017-06-10 10:07:39 +1000

    Nullable Column

--- database/migrations/2017_06_09_063459_AddRemarkColumn.php
+++ database/migrations/2017_06_09_063459_AddRemarkColumn.php
@@ -14,7 +14,7 @@ class AddRemarkColumn extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('remark');
+            $table->string('remark')->nullable();
         });
     }
 
