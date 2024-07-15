6c4a589d71c2020320e303ffecc0bf7b7d6944b6
Alex <github@sinn.io>
2024-07-15 21:41:10 +0100

    wip

--- database/migrations/2017_07_12_062658_RenamedPlayerIDColumn.php
+++ database/migrations/2017_07_12_062658_RenamedPlayerIDColumn.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddedPlayerIDColumn extends Migration
+class RenamedPlayerIDColumn extends Migration
 {
     /**
      * Run the migrations.
