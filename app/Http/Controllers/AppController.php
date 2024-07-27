100371ecc9909dcee135ff7df62f9baea508dcbb
Alex <github@sinn.io>
2024-07-27 12:20:50 +0100

    fixes tests

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -99,7 +99,7 @@ class AppController extends Controller
         if (app()->environment(['testing', 'local'])) {
             return 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
         } elseif (app()->environment(['staging'])) {
-            return 'https://github.com/sifex/scarlet/releases/download/v2.0.0-alpha1/scarlet-windows-latest.zip';
+            return 'https://github.com/sifex/scarlet/releases/download/v2.0.0-alpha2/scarlet-windows-latest.zip';
         } else {
             $github_release_information = GitHub::connection('main')->api('repo')->releases()->latest('sifex', 'scarlet');
             $assets = collect($github_release_information)->get('assets');
